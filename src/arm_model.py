00529f1fbfe2ca6c867c8431a804ff56d8fb259e
Joram Keijser <joramkeijser@gmail.com>
2021-02-10 10:48:38 +0100

    Adds comments to arm model

--- src/arm_model.py
+++ src/arm_model.py
@@ -24,7 +24,6 @@ a3 = I2
 theta0 = np.array([10, 143.54]) * np.pi/180
 x0 = np.concatenate((theta0, np.zeros((2, ))))
 
-
 def get_M(theta):
     c = np.cos(theta[1])
     return N * np.array([[a1 + 2*a2*c, a3 + a2*c], 
@@ -38,29 +37,44 @@ def get_Chi(theta, thetadot):
 B = np.array([[0.05, 0.025], [0.025, 0.05]]) * N
 
 def continuous_dynamics(x, m):
-    # returns xdot = f(x,m)
-    # x = [theta1, theta2, thetadot1, thetadot2]
+    """
+    dx/dt = f(x, m)
+    x: [theta1, theta2, thetadot1, thetadot2]
+    u: [u1, u2]
+    """
     theta, thetadot = x[:2], x[2:]
     Minv = np.linalg.inv(get_M(theta))
     Chi = get_Chi(theta, thetadot)
     return np.concatenate((thetadot, Minv@(m - Chi -B@thetadot)))
     
 def discrete_dynamics(x, m):
+    """
+    x[t+1] = f(x[t], m[t])
+    Fw Euler approximation of continuous dynamics
+    """
     dt = .1
     xnew = x + dt*continuous_dynamics(x, m)
     return xnew, xnew
 
 def get_position(x):
+    """
+    Compute hand position from state
+    """
     theta = x[:2]
     y1 = L1 * np.array([np.cos(theta[0]), np.sin(theta[0])])
     y2 = L2 * np.array([np.cos(theta.sum()), np.sin(theta.sum())])
     return y1 + y2
 
+# Get position over entire trajectory (time steps, 4)
 get_position_trj = vmap(get_position, in_axes=0)
+# Position over multiple trials (batch size, time steps, 4)
+get_position_batch  = vmap(get_position_trj)
 
 def rollout(x0, m_trj):
-    # x0: (4, ) 
-    # m_trj: (time steps, 4)
+    """
+    x0: (4, )
+    m_trj: (time steps, 2)
+    """
     time_steps = m_trj.shape[0]
     _, x_trj = scan(discrete_dynamics, x0, m_trj)
     y_trj = get_position_trj(x_trj)
